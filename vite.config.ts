bd862ef8fdf35fb8b239e16e405dbd9bf2abac1e
Arif Uddin <arifunctg@gmail.com>
2024-08-31 21:54:03 +0600

    chore: unnecessary  comment removed

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
   resolve: {
