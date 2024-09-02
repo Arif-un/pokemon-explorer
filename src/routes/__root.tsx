69531c67151191c48a876c0622c0db7b0adff6c5
Arif Uddin <arifunctg@gmail.com>
2024-09-02 09:02:03 +0600

    feat: optimize theme handling and add navigation components
    - Refactored theme handling to use the `useAtomValue` hook from `jotai`
    - Added `NavBar` and `Footer` components for navigation
    - Updated styling classes and attributes for the main container

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,10 @@
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { useAtom } from 'jotai'
-import { MoonStar, Sun } from 'lucide-react'
+import { useAtomValue } from 'jotai'
 
-import Button from '@/components/ui/button'
+import Footer from '@/components/footer/footer'
+import NavBar from '@/components/nav-bar/nav-bar'
 import themeAtom from '@/global-states/theme-atom'
 
 export const Route = createRootRoute({
@@ -12,26 +13,24 @@ export const Route = createRootRoute({
 })
 
 function RootComponent() {
-  const [theme, setTheme] = useAtom(themeAtom)
+  const theme = useAtomValue(themeAtom)
   const isDark = theme === 'dark'
   const darkThemeClass = isDark ? 'dark' : ''
 
-  const changeTheme = () => {
-    setTheme(isDark ? 'light' : 'dark')
-  }
-
   return (
-    <main className={`${darkThemeClass} dark:bg-slate-950 h-full transition-all`}>
-      <Button className="" icon onClick={changeTheme} rounded>
-        {isDark ? <MoonStar size={18} /> : <Sun size={18} />}
-      </Button>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-      </div>
-      <Outlet />
+    <div
+      className={`flex flex-col min-h-screen ${darkThemeClass} bg-slate-200 dark:bg-slate-950 transition-all`}
+    >
+      <NavBar />
+
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+
+      <Footer />
+
+      <ReactQueryDevtools buttonPosition="top-right" />
       <TanStackRouterDevtools />
-    </main>
+    </div>
   )
 }
