09b4d5bdd5e4c101a707aef9200617306665fc26
Arif Uddin <arifunctg@gmail.com>
2024-09-02 08:50:56 +0600

    feat: add card skeleton loader component
    - Added a new card skeleton loader component for displaying loading placeholders for cards
    - Includes a Pokeball skeleton icon

---
+++ src/components/loaders/card-skeleton-loader/card-skeleton-loader.tsx
@@ -0,0 +1,17 @@
+import PokeballSkeleton from './pokeball-skeleton'
+
+interface CardSkeletonLoaderProps {
+  totalCard?: number
+}
+
+export default function CardSkeletonLoader({ totalCard = 1 }: CardSkeletonLoaderProps) {
+  return Array.from({ length: totalCard }).map((_, index) => (
+    <div
+      key={index}
+      data-testid="card-skeleton-loader"
+      className="w-60 h-64 m-3 flex justify-center items-center shadow border border-slate-300 text-slate-400 bg-slate-300 dark:bg-slate-800 dark:text-slate-950 dark:border-slate-700 rounded-3xl animate-pulse "
+    >
+      <PokeballSkeleton />
+    </div>
+  ))
+}
