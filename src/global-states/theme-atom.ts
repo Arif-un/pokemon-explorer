2a244414dd9c96de8e32f80280784f8f6cdd09b4
Arif Uddin <arifunctg@gmail.com>
2024-09-05 10:06:33 +0600

    refactor: update themeAtom default value to 'dark'

--- src/global-states/theme-atom.ts
+++ src/global-states/theme-atom.ts
@@ -2,6 +2,6 @@ import { atomWithStorage } from 'jotai/utils'
 
 type Theme = 'light' | 'dark'
 
-const themeAtom = atomWithStorage<Theme>('theme', 'light')
+const themeAtom = atomWithStorage<Theme>('theme', 'dark')
 
 export default themeAtom
