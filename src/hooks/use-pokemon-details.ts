140b5b149bc4d19872edd0d17eb475ebc79c714b
Arif Uddin <arifunctg@gmail.com>
2024-09-05 10:05:03 +0600

    refactor: update usePokemonDetails hook to support data selection
    Simplified and enhanced the hook to support data selection using a generic type parameter.

--- src/hooks/use-pokemon-details.ts
+++ src/hooks/use-pokemon-details.ts
@@ -1,11 +1,17 @@
 import { useQuery } from '@tanstack/react-query'
 
 import { fetchPokemonDetails } from '@/services/pokemon-api'
+import { type PokemonDetailsResponse } from '@/types/Pokemon'
 
-export const usePokemonDetails = (name: string) => {
-  const { data, isLoading } = useQuery({
+export const usePokemonDetails = <SelectedData = PokemonDetailsResponse>(
+  name: string,
+  select: (data: PokemonDetailsResponse) => SelectedData
+) => {
+  const { data, isLoading, isError, error } = useQuery<PokemonDetailsResponse, Error, SelectedData>({
     queryKey: ['pokemon', name],
-    queryFn: () => fetchPokemonDetails(name)
+    queryFn: () => fetchPokemonDetails(name),
+    select
   })
-  return { pokemonDetails: data, isLoading }
+
+  return { data: data as SelectedData, isLoading, isError, error }
 }
