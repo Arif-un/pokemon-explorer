0ec21189a68d8913572db3fa447bd237dd8c664c
Arif Uddin <arifunctg@gmail.com>
2024-11-27 19:25:47 +0600

    chore: update test

--- src/utils/query-helpers.test.ts
+++ src/utils/query-helpers.test.ts
@@ -106,7 +106,12 @@ describe('preparePokemonDetails', () => {
     expect(result.id).toBe(1)
     expect(result.image).toBe('official_artwork_shiny.png')
     expect(result.types).toEqual([
-      { name: 'grass', color: '#7AC74C', icon: '/src/assets/type-icons/grass.svg' }
+      {
+        name: 'grass',
+        color: '#7AC74C',
+        // eslint-disable-next-line max-len
+        icon: "data:image/svg+xml,%3csvg%20fill='none'%20height='512'%20viewBox='0%200%20512%20512'%20width='512'%20xmlns='http://www.w3.org/2000/svg'%3e%3cpath%20clip-rule='evenodd'%20d='m97.4121%20440.649c-1.7574-1.653-3.4954-3.338-5.2132-5.056-90.68455-90.684-90.68453-237.713%200-328.397%2090.6841-90.6849%20379.6401-96.7516%20379.6401-96.7516s39.442%20334.4646-51.242%20425.1486c-80.54%2080.54-205.522%2089.55-296.005%2027.031l72.908-89.471%20116.55-25.163-95.139-9.511%2060.462-61.562%2068.824-15.077-54.422-16.117%2054.422-98.176-77.41%2086.828-29.893-42.183%2010.523%2069.648-53.917%2060.782-24.993-76.9v102.268z'%20fill='%23fff'%20fill-rule='evenodd'/%3e%3c/svg%3e"
+      }
     ])
     expect(result.abilities).toEqual(['overgrow'])
     expect(result.baseStats).toEqual([{ name: 'hp', value: 45, max: 255, color: '#ff0000' }])
